cca0ce6075df98106a406eb271633c55ac461fee
John Slowik <john@jslowik.com>
2020-06-27 20:38:15 -0500

    Trying some new formatting

--- AccessAnalytics.Web/ClientApp/src/views/Places.vue
+++ AccessAnalytics.Web/ClientApp/src/views/Places.vue
@@ -1,27 +1,31 @@
 <template>
-  <div class="container">
-    <h1>Places</h1>
-    <button v-on:click="getPlace">Click me</button>
-    <div v-text="name"></div>
-  </div>
+    <div class="container">
+        <h1>Places</h1>
+        <button v-on:click="getPlace">Click me</button>
+        <div v-text="name"></div>
+    </div>
 </template>
 
 <script>
-import axios from "axios";
+    import axios from "axios";
 
-export default {
-  name: "Places",
-  data() {
-    return {
-      name: ""
+    export default {
+        name: "Places",
+        data() {
+            return {
+                name: ""
+            };
+        },
+        methods: {
+            getPlace() {
+                axios.get("/api/PlaceApi/GetPlace").then(
+                    response => (
+                        this.name = response.data.name
+                    )
+                );
+            }
+        }
     };
-  },
-  methods: {
-    getPlace() {
-      axios.get("/api/PlaceApi/GetPlace").then(response => (this.name = response.data.name));
-    }
-  }
-};
 </script>
 
 <style scoped>
